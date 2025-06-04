390f1a495fd4efe92a5197b2151dc75584c1f785
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-06-04 23:52:34 +0530

    feat: added campaign mongoose model.
    - Installed uuid package.

---
+++ src/models/campaign.model.ts
@@ -0,0 +1,30 @@
+import { model, Model, Schema } from 'mongoose';
+import { CampaignDomainModel, campaignStatusEnum } from '../dto/campaign.dto.js';
+import { v4 as uuidV4 } from 'uuid';
+
+const campaignSchema = new Schema<CampaignDomainModel>({
+    _id: {
+        type: String,
+        default: uuidV4,
+    },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    status: {
+        type: String,
+        enum: campaignStatusEnum,
+        required: true,
+        default: campaignStatusEnum.ACTIVE,
+    },
+    leads: {
+        type: [String],
+        required: true,
+        default: []
+    },
+    accountIDs: {
+        type: [String],
+        required: true,
+        default: []
+    },
+}, { collection: 'campaigns', timestamps: true });
+
+export const Campaign: Model<CampaignDomainModel> = model('Campaign', campaignSchema);
