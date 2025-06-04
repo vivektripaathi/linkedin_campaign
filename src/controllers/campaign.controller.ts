e7d7156d2805932ac17410088e30a6c80015ced5
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-06-05 01:24:14 +0530

    feat: add get campaign by id controller function.

--- src/controllers/campaign.controller.ts
+++ src/controllers/campaign.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { wrapAsync } from '../utils/errorHandler.js';
 import { successResponse, errorResponse } from '../utils/apiResponse.js';
-import { CreateCampaignRequestDto } from '../dto/campaign.dto.js';
+import { CreateCampaignRequestDto, GetCampaignByIdRequestDto } from '../dto/campaign.dto.js';
 import { validateAndParseDto } from '../utils/validateAndParseDto.js';
 import * as campaignDao from '../dao/campaign.dao.js';
 
@@ -9,6 +9,20 @@ export const createCampaign = wrapAsync(async (req: Request, res: Response) => {
     const [createCampaignRequest, errors] = await validateAndParseDto(CreateCampaignRequestDto, req.body);
     if (errors.length) return errorResponse(res, errors.join(', '), 400);
 
-    const campaign = await campaignDao.create(createCampaignRequest);
-    return successResponse(res, campaign, 'Campaign created', 201);
+    const createdCampaign = await campaignDao.create(createCampaignRequest);
+    return successResponse(res, createdCampaign, 'Campaign created', 201);
+});
+
+
+export const getCampaignById = wrapAsync(async (req: Request, res: Response) => {
+    const [getCampaignByIdRequest, errors] = await validateAndParseDto(GetCampaignByIdRequestDto, req.params);
+    if (errors.length) return errorResponse(res, errors.join(', '), 400);
+
+    const campaignId = getCampaignByIdRequest.id;
+    const campaign = await campaignDao.getById(campaignId);
+    if (!campaign) {
+        return errorResponse(res, `Campaign with id ${campaignId} does not exists`, 404);
+    }
+
+    return successResponse(res, campaign);
 });
