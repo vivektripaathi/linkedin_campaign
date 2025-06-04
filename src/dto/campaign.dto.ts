ed87c8077bfba51ad4b4ab6708ca76f359e6c9b9
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-06-04 23:52:07 +0530

    feat: added create campaign request dto

--- src/dto/campaign.dto.ts
+++ src/dto/campaign.dto.ts
@@ -1,3 +1,6 @@
+import { IsEnum, IsString, IsArray, ArrayNotEmpty } from 'class-validator';
+import { Expose } from 'class-transformer';
+
 export enum campaignStatusEnum {
     ACTIVE = "active",
     INACTIVE = "inactive",
@@ -13,4 +16,29 @@ export class CampaignDomainModel {
     createdAt!: string;
     updatedAt!: string;
     __v!: number;
+}
+
+export class  CreateCampaignRequestDto implements Omit<CampaignDomainModel, '_id' | 'createdAt' | 'updatedAt' | '__v'>{
+    @Expose()
+    @IsString()
+    name!: string;
+
+    @Expose()
+    @IsString()
+    description!: string;
+
+    @Expose()
+    @IsEnum(campaignStatusEnum)
+    status!: campaignStatusEnum;
+
+    @Expose()
+    @IsArray()
+    @ArrayNotEmpty()
+    @IsString({ each: true })
+    leads!: string[];
+
+    @Expose()
+    @IsArray()
+    @IsString({ each: true })
+    accountIDs!: string[];
 }
