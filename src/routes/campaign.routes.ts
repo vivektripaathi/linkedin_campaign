a0c120111344f7fe09655afca87708495410fe03
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-06-24 02:38:33 +0530

    refactor: updated campaign routing to align with class based controller.

--- src/routes/campaign.routes.ts
+++ src/routes/campaign.routes.ts
@@ -1,18 +1,12 @@
 import { Router } from 'express';
-import {
-  getAllCampaigns,
-  getCampaignById,
-  createCampaign,
-  updateCampaignById,
-  deleteCampaign
-} from '../controllers/campaign.controller.js';
+import { CampaignController } from '../controllers/campaign.controller.js';
+import { CampaignDao } from '../dao/campaign.dao.js';
+import { wrapAsync } from '../utils/errorHandler.js';
 
-const router = Router();
+const campaignController = new CampaignController(new CampaignDao());
 
-router.get('/', getAllCampaigns);
-router.get('/:id', getCampaignById);
-router.post('/', createCampaign);
-router.put('/:id', updateCampaignById);
-router.delete('/:id', deleteCampaign);
-
-export default router;
+export default Router().post('/', wrapAsync(campaignController.createCampaign.bind(campaignController)))
+    .get('/:id', wrapAsync(campaignController.getCampaignById.bind(campaignController)))
+    .put('/:id', wrapAsync(campaignController.updateCampaignById.bind(campaignController)))
+    .get('/', wrapAsync(campaignController.getAllCampaigns.bind(campaignController)))
+    .delete('/:id', wrapAsync(campaignController.deleteCampaign.bind(campaignController)));
