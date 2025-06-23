15199bbdae365c9af16e9a8ca0e08e5bd7de5143
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-06-24 02:37:56 +0530

    refactor(controllers): converted function based to object oriented campaign controllers.

--- src/controllers/campaign.controller.ts
+++ src/controllers/campaign.controller.ts
@@ -1,55 +1,97 @@
 import { Request, Response } from 'express';
-import { wrapAsync } from '../utils/errorHandler.js';
 import { successResponse, errorResponse } from '../utils/apiResponse.js';
-import { CreateCampaignRequestDto, UpdateCampaignRequestDto } from '../dto/campaign.dto.js';
+import { CampaignDao } from '../dao/campaign.dao.js';
 import { validateAndParseDto } from '../utils/validateAndParseDto.js';
-import * as campaignDao from '../dao/campaign.dao.js';
+import {
+    CreateCampaignRequestDto,
+    CampaignDomainModel,
+    CampaignResponseDto,
+    UpdateCampaignRequestDto
+} from '../dto/campaign.dto.js';
 import { ParamIdRequestDto } from '../dto/base.dto.js';
 
-export const createCampaign = wrapAsync(async (req: Request, res: Response) => {
-    const [createCampaignRequest, errors] = await validateAndParseDto(CreateCampaignRequestDto, req.body);
-    if (errors.length) return errorResponse(res, errors.join(', '), 400);
+export class CampaignController {
+    constructor(private readonly campaignDao: CampaignDao) { }
 
-    const createdCampaign = await campaignDao.create(createCampaignRequest);
-    return successResponse(res, createdCampaign, 'Campaign created', 201);
-});
-
-
-export const getCampaignById = wrapAsync(async (req: Request, res: Response) => {
-    const [getCampaignByIdRequest, errors] = await validateAndParseDto(ParamIdRequestDto, req.params);
-    if (errors.length) return errorResponse(res, errors.join(', '), 400);
-
-    const campaignId = getCampaignByIdRequest.id;
-    const campaign = await campaignDao.getById(campaignId);
-    if (!campaign) {
-        return errorResponse(res, `Campaign with id ${campaignId} does not exists`, 404);
+    private prepareDomainModelFromCreateDto(dto: CreateCampaignRequestDto): CampaignDomainModel {
+        return {
+            name: dto.name,
+            description: dto.description,
+            status: dto.status,
+            leads: dto.leads,
+            accountIDs: dto.accountIDs,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            deletedAt: dto.deletedAt ?? null,
+            _id: undefined as any
+        };
     }
 
-    return successResponse(res, campaign);
-});
 
-export const updateCampaignById = wrapAsync(async (req: Request, res: Response) => {
-    const [updateCampaignParam, paramErrors] = await validateAndParseDto(ParamIdRequestDto, req.params);
-    if (paramErrors.length) return errorResponse(res, paramErrors.join(', '), 400);
+    async createCampaign(req: Request, res: Response) {
+        //create campaign request validation
+        const [createRequest, errors] = await validateAndParseDto(CreateCampaignRequestDto, req.body);
+        if (errors.length) return errorResponse(res, errors.join(', '), 400);
 
-    const [updateCampaignRequest, requestErrors] = await validateAndParseDto(UpdateCampaignRequestDto, req.body);
-    if (requestErrors.length) return errorResponse(res, requestErrors.join(', '), 400);
+        //prepare domain model from create request and create campaign
+        const createCampaignPayload = this.prepareDomainModelFromCreateDto(createRequest);
+        const createdCampaign = await this.campaignDao.create(createCampaignPayload);
 
-    const campaign = await campaignDao.updateById(updateCampaignParam.id, updateCampaignRequest);
-    if (!campaign) return errorResponse(res, `Campaign with id ${updateCampaignParam.id} does not exists`, 404);
+        //validate and parse response and return response
+        const [response, responseErrors] = await validateAndParseDto(CampaignResponseDto, createdCampaign);
+        if (responseErrors.length) return errorResponse(res, responseErrors.join(', '), 500);
+        return successResponse(res, response, 'Campaign created', 201);
+    };
 
-    return successResponse(res, campaign, 'Campaign updated');
-});
 
-export const getAllCampaigns = wrapAsync(async (_: Request, res: Response) => successResponse(res, (await campaignDao.getAll())));
+    async getCampaignById(req: Request, res: Response) {
+        const [getRequest, errors] = await validateAndParseDto(ParamIdRequestDto, req.params);
+        if (errors.length) return errorResponse(res, errors.join(', '), 400);
 
-export const deleteCampaign = wrapAsync(async (req: Request, res: Response) => {
-    const [deleteCampaignByIdParam, errors] = await validateAndParseDto(ParamIdRequestDto, req.params);
-    if (errors.length) return errorResponse(res, errors.join(', '), 400);
+        const campaign = await this.campaignDao.getById(getRequest.id);
+        if (!campaign) return errorResponse(res, `Campaign with id ${getRequest.id} not found`, 404);
 
-    const campaign = await campaignDao.updateById(deleteCampaignByIdParam.id, { deletedAt: new Date() });
-    if (!campaign) return errorResponse(res, `Campaign with id ${deleteCampaignByIdParam.id} does not exists`, 404);
+        const [response, responseErrors] = await validateAndParseDto(CampaignResponseDto, campaign);
+        if (responseErrors.length) return errorResponse(res, responseErrors.join(', '), 500);
+        return successResponse(res, response);
+    };
 
-    if (!campaign) return errorResponse(res, 'Campaign not found', 404);
-    return successResponse(res, null, 'Campaign soft-deleted');
-});
+
+
+    async updateCampaignById(req: Request, res: Response) {
+        const [updateParam, paramErrors] = await validateAndParseDto(ParamIdRequestDto, req.params);
+        if (paramErrors.length) return errorResponse(res, paramErrors.join(', '), 400);
+        const [updateRequest, dtoErrors] = await validateAndParseDto(UpdateCampaignRequestDto, req.body);
+        if (dtoErrors.length) return errorResponse(res, dtoErrors.join(', '), 400);
+
+        const updated = await this.campaignDao.updateById(updateParam.id, updateRequest);
+        if (!updated) return errorResponse(res, `Campaign with id ${updateParam.id} not found`, 404);
+
+        const [response, responseErrors] = await validateAndParseDto(CampaignResponseDto, updated);
+        if (responseErrors.length) return errorResponse(res, responseErrors.join(', '), 500);
+        return successResponse(res, response, 'Campaign updated');
+    };
+
+
+    async getAllCampaigns(_: Request, res: Response) {
+        const campaigns = await this.campaignDao.getAll();
+
+        const validatedList: CampaignResponseDto[] = [];
+        for (const campaign of campaigns) {
+            const [validated, errors] = await validateAndParseDto(CampaignResponseDto, campaign);
+            if (errors.length) return errorResponse(res, errors.join(', '), 500);
+            validatedList.push(validated);
+        }
+        return successResponse(res, validatedList);
+    };
+
+    async deleteCampaign(req: Request, res: Response) {
+        const [deleteParams, errors] = await validateAndParseDto(ParamIdRequestDto, req.params);
+        if (errors.length) return errorResponse(res, errors.join(', '), 400);
+
+        const updated = await this.campaignDao.updateById(deleteParams.id, { deletedAt: new Date() });
+        if (!updated) return errorResponse(res, `Campaign with id ${deleteParams.id} not found`, 404);
+
+        return successResponse(res, null, 'Campaign soft-deleted');
+    };
+}
