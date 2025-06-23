9d0b6e65cfbe1c1d047c169a1622e5d10519c723
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-06-24 02:36:55 +0530

    feat: introduced class based base and campaign DAOs.

---
+++ src/dao/base.dao.ts
@@ -0,0 +1,27 @@
+// base.dao.ts
+import { Model } from 'mongoose';
+import { UUIDTypes } from 'uuid';
+
+export class BaseDao<T> {
+    constructor(protected readonly model: Model<T>) { }
+
+    async create(data:  T,): Promise<T> {
+        return await this.model.create(data);
+    }
+
+    async getById(id: UUIDTypes): Promise<T | null> {
+        return await this.model.findOne({ _id: id, deletedAt: null }) as T | null;
+    }
+
+    async updateById(id: UUIDTypes, data: Partial<T>): Promise<T | null> {
+        return await this.model.findOneAndUpdate(
+            { _id: id, deletedAt: null },
+            data,
+            { new: true }
+        ) as T | null;
+    }    
+
+    async getAll(): Promise<T[]> {
+        return await this.model.find({ deletedAt: null }) as T[];
+    }
+}
--- src/dao/campaign.dao.ts
+++ src/dao/campaign.dao.ts
@@ -1,48 +1,9 @@
-import { CreateCampaignRequestDto, CampaignResponseDto, UpdateCampaignRequestDto } from "../dto/campaign.dto.js";
-import { validateAndParseDto } from "../utils/validateAndParseDto.js";
+import { BaseDao } from './base.dao.js';
 import { Campaign } from '../models/campaign.model.js';
-import { UUIDTypes } from "uuid";
+import { CampaignDomainModel } from '../dto/campaign.dto.js';
 
-export const create = async (campaignRequest: CreateCampaignRequestDto): Promise<CampaignResponseDto> => {
-    const createdCampaign = await Campaign.create(campaignRequest);
-
-    const [createdCampaignResponse, errors] = await validateAndParseDto(CampaignResponseDto, createdCampaign);
-    if (errors.length) throw (errors.join(', '));
-
-    return createdCampaignResponse;
-};
-
-export const getById = async (campaignId: UUIDTypes): Promise<CampaignResponseDto | null> => {
-    const campaignDbEntry = await Campaign.findOne({ _id: campaignId, deletedAt: null });
-    if (campaignDbEntry) {
-        const [campaignResponse, errors] = await validateAndParseDto(CampaignResponseDto, campaignDbEntry);
-        if (errors.length) throw (errors.join(', '));
-        return campaignResponse;
+export class CampaignDao extends BaseDao<CampaignDomainModel> {
+    constructor() {
+        super(Campaign);
     }
-    return campaignDbEntry;
 }
-
-export const updateById = async (campaignId: UUIDTypes, updateRequest: UpdateCampaignRequestDto): Promise<CampaignResponseDto | null> => {
-    const campaignExists = !!(await getById(campaignId));
-    if (!campaignExists) return null;
-
-    const updatedCampaign = await Campaign.findByIdAndUpdate(campaignId, updateRequest, { new: true });
-
-    const [updatedCampaignResponse, errors] = await validateAndParseDto(CampaignResponseDto, updatedCampaign as CampaignResponseDto);
-    if (errors.length) throw (errors.join(', '));
-
-    return updatedCampaignResponse;
-}
-
-export const getAll = async (): Promise<CampaignResponseDto[]> => {
-    const campaignDbEntries = await Campaign.find({ deletedAt: null });
-
-    const validatedCampaigns: CampaignResponseDto[] = [];
-    for (const campaign of campaignDbEntries) {
-        const [validatedCampaign, errors] = await validateAndParseDto(CampaignResponseDto, campaign);
-        if (errors.length) throw new Error(errors.join(', '));
-        validatedCampaigns.push(validatedCampaign);
-    }
-
-    return validatedCampaigns;
-};
