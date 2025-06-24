4f011ec989c69ce440616d47cd4dddd5e2fd5fa9
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-06-24 11:56:16 +0530

    feat(utils): add AiApiKeyMissingException.

--- src/utils/exceptions.ts
+++ src/utils/exceptions.ts
@@ -7,3 +7,10 @@ export class AppError extends Error {
         Object.setPrototypeOf(this, AppError.prototype)
     }
 }
+
+export class AiApiKeyMissingException extends AppError {
+    constructor(message = "AI API key is missing. Pass it using environment variable.") {
+        super(message, 401)
+        Object.setPrototypeOf(this, AiApiKeyMissingException.prototype)
+    }
+}
