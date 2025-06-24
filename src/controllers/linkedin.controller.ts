785fbc1415c768f5db7a8c749f34d0a6e8cf348b
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-06-24 12:12:50 +0530

    feat(controller): add linkedin message controller.

---
+++ src/controllers/linkedin.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { successResponse, errorResponse } from '../utils/apiResponse.js';
+import { validateAndParseDto } from '../utils/validateAndParseDto.js';
+import { ParamIdRequestDto } from '../dto/base.dto.js';
+import { LinkedInMessageService } from '../services/linkedin.service.js';
+import { LinkedInProfileRequestDto } from '../dto/linkedin.dto.js';
+
+export class LinkedInMessageController {
+    private linkedInMessageService: LinkedInMessageService
+
+    constructor() {
+        this.linkedInMessageService = new LinkedInMessageService()
+    }
+
+
+    async getPersonalizedMessage(req: Request, res: Response) {
+        const [profile, errors] = await validateAndParseDto(LinkedInProfileRequestDto, req.body ?? {});
+        if (errors.length) return errorResponse(res, errors.join(', '), 400);
+
+        const message = await this.linkedInMessageService.generatePersonalizedMessage(profile);
+        return successResponse(res, {message}, 200);
+    }
+}
