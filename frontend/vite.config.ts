925095b91d5e60af667feff9355067ac9cad7762
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-06-25 03:12:05 +0530

    chore: updated vite config to add server proxy.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,17 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());;
+
+  return {
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    server: {
+      proxy: {
+        "/api": `${env.VITE_API_BASE_URL}`,
+      },
+    },
+  };
 });
