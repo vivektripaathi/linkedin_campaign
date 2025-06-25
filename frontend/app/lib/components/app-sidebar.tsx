999bd3e937c8b39740ce0801007572230efdba4a
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-06-25 05:33:37 +0530

    fix: minor sidebar fixes.

--- frontend/app/lib/components/app-sidebar.tsx
+++ frontend/app/lib/components/app-sidebar.tsx
@@ -112,9 +112,9 @@ export function AppSidebar({ children }: { children: React.ReactNode }) {
                                         className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                                     >
                                         <Avatar className="h-8 w-8 rounded-lg">
-                                            <AvatarImage src={data.user.name} />
+                                            <AvatarImage />
                                             <AvatarFallback className="rounded-lg">
-                                                CN
+                                                VT
                                             </AvatarFallback>
                                         </Avatar>
                                         <div className="grid flex-1 text-left text-sm leading-tight">
@@ -137,11 +137,9 @@ export function AppSidebar({ children }: { children: React.ReactNode }) {
                                     <DropdownMenuLabel className="p-0 font-normal">
                                         <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                                             <Avatar className="h-8 w-8 rounded-lg">
-                                                <AvatarImage
-                                                    src={data.user.name}
-                                                />
+                                                <AvatarImage />
                                                 <AvatarFallback className="rounded-lg">
-                                                    CN
+                                                    VT
                                                 </AvatarFallback>
                                             </Avatar>
                                             <div className="grid flex-1 text-left text-sm leading-tight">
