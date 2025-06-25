7c65d4a1e3692402ed781658277f98727de0c97b
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-06-25 08:38:57 +0530

    feat: added toaster in layout.

--- frontend/app/lib/components/Layout/LayoutWithSidebar.tsx
+++ frontend/app/lib/components/Layout/LayoutWithSidebar.tsx
@@ -1,6 +1,7 @@
 import { AppSidebar } from "../app-sidebar";
 import { SidebarProvider } from "@components/ui/sidebar";
 import { AppSidebarInset } from "../app-sidebar-inset";
+import { Toaster } from "sonner";
 
 export default function LayoutWithSidebar({
     children,
@@ -9,6 +10,7 @@ export default function LayoutWithSidebar({
 }) {
     return (
         <SidebarProvider defaultOpen={true}>
+            <Toaster richColors position="top-right" />
             <AppSidebar>
                 <AppSidebarInset>{children}</AppSidebarInset>
             </AppSidebar>
