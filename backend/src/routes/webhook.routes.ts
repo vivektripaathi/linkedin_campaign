bed808e76a50567b41e04fe55ced97dc479968b3
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-07-06 01:48:08 +0530

    feat(webhook): integrate ChatController and MessageController into WebhookController for enhanced message handling

--- backend/src/routes/webhook.routes.ts
+++ backend/src/routes/webhook.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from 'express';
 import { wrapAsync } from '../utils/errorHandler.js';
 import { WebhookController } from '../controllers/webhook.controller.js';
+import { ChatController } from '../controllers/chat.controller.js';
+import { MessageController } from '../controllers/message.controller.js';
+import { ChatDao } from '../dao/chat.dao.js';
+import { MessageDao } from '../dao/message.dao.js';
 
-const webhookController = new WebhookController();
+const webhookController = new WebhookController(
+    new ChatController(new ChatDao),
+    new MessageController(new MessageDao)
+);
 
 export default Router()
     .post('/on_new_message', wrapAsync(webhookController.onNewMessage.bind(webhookController)))
