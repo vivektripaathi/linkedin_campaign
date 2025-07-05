4e68a2801822147e9b2bc1758a21cc9af91152f7
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-07-05 16:54:49 +0530

    feat(mock-data): add mock chat data and messages for testing chat functionality.

---
+++ frontend/app/lib/mock-chat-data.ts
@@ -0,0 +1,103 @@
+import type { ChatViewInterface, MessageViewInterface } from "@lib/types"
+
+export const mockChats: ChatViewInterface[] = [
+  {
+    id: 'PaLI4eOPUfaF10OzObfdiA',
+    accountId: 't5fp2R9rSyCQGivyj7zSgA',
+    attendeeProviderId: 'ACoAADh2JUwBDGT-3vhpYi-1Ndjl384WPsUhwQ8',
+    attendeeName: 'Vivek Tripathi',
+    attendeePictureUrl: 'https://media.licdn.com/dms/image/v2/D5603AQHJPHFT4BeH-A/profile-displayphoto-shrink_400_400/B56Za08.2mH0Ao-/0/1746792608445?e=1756944000&v=beta&t=idq3zA4qQML2O9AnNdZsO6A-iO81zA3u93q9nCVyWJg',
+    unreadCount: 6,
+    lastMessage: "Hey Cristian, this is vivek. How may I help you?"
+  },
+  {
+    id: 'VnvoqoyDXeO5PP8B5uLCbg',
+    accountId: 't5fp2R9rSyCQGivyj7zSgA',
+    attendeeProviderId: 'ACoAAETZP34BuJKQtwb3qNLHR2LwzwL6zCqP48k',
+    attendeeName: 'Vijay Tripathi',
+    attendeePictureUrl: undefined
+  }
+]
+
+export const mockChatMessages: MessageViewInterface[] = [
+  {
+    id: 'R8N3gfZJVrWzhNhu-9E-cQ',
+    text: 'Hey Cristian, this is vivek. How may I help you?\n',
+    chatId: 'PaLI4eOPUfaF10OzObfdiA',
+    timestamp: '2025-07-02T19:44:19.589Z',
+    accountId: 't5fp2R9rSyCQGivyj7zSgA',
+    senderProviderId: 'ACoAADh2JUwBDGT-3vhpYi-1Ndjl384WPsUhwQ8'
+  },
+  {
+    id: 'ij_rWZefVUWHAGEKEmzfIg',
+    text: 'Hey this side Cristian P.',
+    chatId: 'PaLI4eOPUfaF10OzObfdiA',
+    timestamp: '2025-07-02T19:43:24.009Z',
+    accountId: 't5fp2R9rSyCQGivyj7zSgA',
+    senderProviderId: 'ACoAAFBA9dUBBkgN3_tXHcj3uyjn2EXANH2W3Gg'
+  },
+  {
+    id: 'AHPKE1wjXpKUsLTXZ0jplw',
+    text: 'Ksdl',
+    chatId: 'PaLI4eOPUfaF10OzObfdiA',
+    timestamp: '2025-07-02T17:34:45.024Z',
+    accountId: 't5fp2R9rSyCQGivyj7zSgA',
+    senderProviderId: 'ACoAADh2JUwBDGT-3vhpYi-1Ndjl384WPsUhwQ8'
+  },
+  {
+    id: 'pOtTwnjUVIaMn_3ExvWO5Q',
+    text: 'Hoisdfg',
+    chatId: 'PaLI4eOPUfaF10OzObfdiA',
+    timestamp: '2025-07-02T17:15:50.964Z',
+    accountId: 't5fp2R9rSyCQGivyj7zSgA',
+    senderProviderId: 'ACoAADh2JUwBDGT-3vhpYi-1Ndjl384WPsUhwQ8'
+  },
+  {
+    id: '2PPPWDrwUxG_AAMobhUOoA',
+    text: 'hey',
+    chatId: 'PaLI4eOPUfaF10OzObfdiA',
+    timestamp: '2025-07-02T16:21:30.445Z',
+    accountId: 't5fp2R9rSyCQGivyj7zSgA',
+    senderProviderId: 'ACoAAFBA9dUBBkgN3_tXHcj3uyjn2EXANH2W3Gg'
+  },
+  {
+    id: 'ZKTJjdbHWT-hC5a5sgkqsw',
+    text: 'HI',
+    chatId: 'VnvoqoyDXeO5PP8B5uLCbg',
+    timestamp: '2025-07-02T16:04:53.776Z',
+    accountId: 't5fp2R9rSyCQGivyj7zSgA',
+    senderProviderId: 'ACoAAFBA9dUBBkgN3_tXHcj3uyjn2EXANH2W3Gg'
+  },
+  {
+    id: 'y-d5pedQUNOBV5hs9KZuAg',
+    text: '?',
+    chatId: 'PaLI4eOPUfaF10OzObfdiA',
+    timestamp: '2025-07-01T22:39:08.785Z',
+    accountId: 't5fp2R9rSyCQGivyj7zSgA',
+    senderProviderId: 'ACoAAFBA9dUBBkgN3_tXHcj3uyjn2EXANH2W3Gg'
+  },
+  {
+    id: 'REKBvtriUQ-VSrrTFOYKGg',
+    text: 'Bha',
+    chatId: 'PaLI4eOPUfaF10OzObfdiA',
+    timestamp: '2025-07-01T22:36:31.349Z',
+    accountId: 't5fp2R9rSyCQGivyj7zSgA',
+    senderProviderId: 'ACoAADh2JUwBDGT-3vhpYi-1Ndjl384WPsUhwQ8'
+  },
+  {
+    id: 'qjXdXSyPV4Kz97ZPAWbKjg',
+    text: 'Ddd',
+    chatId: 'PaLI4eOPUfaF10OzObfdiA',
+    timestamp: '2025-07-01T22:34:14.838Z',
+    accountId: 't5fp2R9rSyCQGivyj7zSgA',
+    senderProviderId: 'ACoAADh2JUwBDGT-3vhpYi-1Ndjl384WPsUhwQ8'
+  },
+  {
+    id: 'BeMWiWEyVnWkChw96f4vhg',
+    text: 'Hi',
+    chatId: 'PaLI4eOPUfaF10OzObfdiA',
+    timestamp: '2025-07-01T22:33:43.202Z',
+    accountId: 't5fp2R9rSyCQGivyj7zSgA',
+    senderProviderId: 'ACoAADh2JUwBDGT-3vhpYi-1Ndjl384WPsUhwQ8'
+  }
+]
