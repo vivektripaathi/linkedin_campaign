6248d082bf8770906507bf6f950692516a09b3f3
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2025-07-10 03:14:16 +0530

    feat(sidebar): integrate user authentication in AppSidebar with logout functionality and dynamic user display.

--- frontend/app/lib/components/app-sidebar.tsx
+++ frontend/app/lib/components/app-sidebar.tsx
@@ -8,6 +8,7 @@ import {
     Megaphone,
     Users,
     MessageSquareText,
+    LogOut,
 } from "lucide-react";
 import { Separator } from "@components/ui/separator";
 import { Avatar, AvatarFallback, AvatarImage } from "@components/ui/avatar";
@@ -15,7 +16,10 @@ import { Collapsible, CollapsibleTrigger } from "@components/ui/collapsible";
 import {
     DropdownMenu,
     DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
     DropdownMenuLabel,
+    DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@components/ui/dropdown-menu";
 import {
@@ -31,12 +35,10 @@ import {
 } from "@components/ui/sidebar";
 import { useLocation, useNavigate } from "react-router-dom";
 import { PAGE_TITLES, ROUTES } from "~/routes";
+import { useAuthContext } from "../providers/auth-provider";
+import { getInitials } from "../utils";
 
 const data = {
-    user: {
-        name: "Vivek Tripathi",
-        email: "vivektripathi8005@gmail.com",
-    },
     navMain: [
         {
             title: PAGE_TITLES[ROUTES.CAMPAIGNS],
@@ -62,6 +64,7 @@ const data = {
 };
 
 export function AppSidebar({ children }: { children: React.ReactNode }) {
+    const { user, logout } = useAuthContext();
     const location = useLocation();
     const navigate = useNavigate();
 
@@ -75,7 +78,7 @@ export function AppSidebar({ children }: { children: React.ReactNode }) {
                                 <DropdownMenuTrigger asChild>
                                     <SidebarMenuButton
                                         size="lg"
-                                        className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                                        className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground hover:cursor-pointer"
                                     >
                                         <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
                                             <Megaphone className="size-4" />
@@ -132,20 +135,20 @@ export function AppSidebar({ children }: { children: React.ReactNode }) {
                                 <DropdownMenuTrigger asChild>
                                     <SidebarMenuButton
                                         size="lg"
-                                        className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                                        className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground hover:cursor-pointer"
                                     >
                                         <Avatar className="h-8 w-8 rounded-lg">
                                             <AvatarImage />
                                             <AvatarFallback className="rounded-lg">
-                                                VT
+                                                {getInitials(user?.name as string)}
                                             </AvatarFallback>
                                         </Avatar>
                                         <div className="grid flex-1 text-left text-sm leading-tight">
                                             <span className="truncate font-semibold">
-                                                {data.user.name}
+                                                {user?.name}
                                             </span>
                                             <span className="truncate text-xs">
-                                                {data.user.email}
+                                                {user?.email}
                                             </span>
                                         </div>
                                         <ChevronsUpDown className="ml-auto size-4" />
@@ -162,19 +165,29 @@ export function AppSidebar({ children }: { children: React.ReactNode }) {
                                             <Avatar className="h-8 w-8 rounded-lg">
                                                 <AvatarImage />
                                                 <AvatarFallback className="rounded-lg">
-                                                    VT
+                                                    {getInitials(user?.name as string)}
                                                 </AvatarFallback>
                                             </Avatar>
                                             <div className="grid flex-1 text-left text-sm leading-tight">
                                                 <span className="truncate font-semibold">
-                                                    {data.user.name}
+                                                    {user?.name}
                                                 </span>
                                                 <span className="truncate text-xs">
-                                                    {data.user.email}
+                                                    {user?.email}
                                                 </span>
                                             </div>
                                         </div>
                                     </DropdownMenuLabel>
+                                    <DropdownMenuSeparator />
+                                    <DropdownMenuGroup>
+                                        <DropdownMenuItem
+                                            onClick={logout}
+                                            className="hover:cursor-pointer"
+                                        >
+                                            <LogOut />
+                                            Log out
+                                        </DropdownMenuItem>
+                                    </DropdownMenuGroup>
                                 </DropdownMenuContent>
                             </DropdownMenu>
                         </SidebarMenuItem>
